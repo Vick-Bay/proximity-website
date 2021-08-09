9ea7f51598be81fdfc0d89714c1cc8ff1fc47309
Vick-Bay <vickramb@hotmail.com>
2021-08-09 16:02:23 +0400

    Updates

---
+++ precache-manifest.fc28ce7077b86a9c6eb540e0ede5c19b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39f003ddc7d825cc5192b403a25628c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0582c8dfeeeb6e7784b",
+    "url": "./static/css/main.2d43de5b.chunk.css"
+  },
+  {
+    "revision": "e700be9399ef57ff599a",
+    "url": "./static/js/15.5ea43860.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/15.5ea43860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0582c8dfeeeb6e7784b",
+    "url": "./static/js/main.65f4043a.chunk.js"
+  },
+  {
+    "revision": "eff5f48f9d7653144f2f",
+    "url": "./static/js/reactPlayerDailyMotion.62c8b3ad.chunk.js"
+  },
+  {
+    "revision": "7b365d1ae642538eabed",
+    "url": "./static/js/reactPlayerFacebook.f53faa6a.chunk.js"
+  },
+  {
+    "revision": "530f2af493a641b85fac",
+    "url": "./static/js/reactPlayerFilePlayer.64207e8e.chunk.js"
+  },
+  {
+    "revision": "f09c3220acc26bf51826",
+    "url": "./static/js/reactPlayerKaltura.53bc27f6.chunk.js"
+  },
+  {
+    "revision": "e109ddd743e3a4f104c1",
+    "url": "./static/js/reactPlayerMixcloud.dcffb278.chunk.js"
+  },
+  {
+    "revision": "13f5a40e239e9d4ae713",
+    "url": "./static/js/reactPlayerPreview.cbcc5a1d.chunk.js"
+  },
+  {
+    "revision": "859f0236022127daa526",
+    "url": "./static/js/reactPlayerSoundCloud.08d7f111.chunk.js"
+  },
+  {
+    "revision": "4b3da8f2d547820b2174",
+    "url": "./static/js/reactPlayerStreamable.b4b013d3.chunk.js"
+  },
+  {
+    "revision": "f7da8e6a493d98065064",
+    "url": "./static/js/reactPlayerTwitch.b0d2dbf1.chunk.js"
+  },
+  {
+    "revision": "ea42c9951f6c4aa5eafb",
+    "url": "./static/js/reactPlayerVidyard.b43bf002.chunk.js"
+  },
+  {
+    "revision": "6da148ba4ee02d5cf224",
+    "url": "./static/js/reactPlayerVimeo.b653f5d1.chunk.js"
+  },
+  {
+    "revision": "427a116d5888e74da51f",
+    "url": "./static/js/reactPlayerWistia.365fcdc9.chunk.js"
+  },
+  {
+    "revision": "7cba0eda8a0ef637197e",
+    "url": "./static/js/reactPlayerYouTube.3f3abbff.chunk.js"
+  },
+  {
+    "revision": "7b296a02dd2d8a847e08",
+    "url": "./static/js/runtime-main.9c2e520c.js"
+  },
+  {
+    "revision": "35ba3e0c080cb22ce11be87bea27d66c",
+    "url": "./static/media/Avenir-Black.35ba3e0c.ttf"
+  },
+  {
+    "revision": "1cf2c7d0e7d0694aa0c9850d78765fcd",
+    "url": "./static/media/Avenir-Book.1cf2c7d0.ttf"
+  },
+  {
+    "revision": "aed8836fe0df47954491ee3f619bd552",
+    "url": "./static/media/Avenir-Medium.aed8836f.ttf"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "./static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "./static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "./static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "./static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "7f8857beb98db5b401e8e8246198a8bd",
+    "url": "./static/media/proximity-logo.7f8857be.png"
+  },
+  {
+    "revision": "68a448138b75851cd20ac524537a456c",
+    "url": "./static/media/service.68a44813.jpg"
+  },
+  {
+    "revision": "a4f196ececbc9a2870d83148ff0aaa6c",
+    "url": "./static/media/video-poster.a4f196ec.jpg"
+  },
+  {
+    "revision": "fff76e116131242571e5f5e2dc239a1b",
+    "url": "./static/media/video.fff76e11.mp4"
+  }
+]);
